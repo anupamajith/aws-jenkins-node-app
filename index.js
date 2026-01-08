af1b164db2e888eeb0ed47775b30f9b28980f244
Ubuntu <ubuntu@ip-172-31-30-199.ec2.internal>
2026-01-08 20:18:33 +0000

    Test webhook auto deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins CI/CD on AWS EC2!');
+  res.send('Webhook auto deploy workingy');
 });
 
 app.listen(3000, () => {
